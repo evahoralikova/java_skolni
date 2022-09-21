8e02e2cbf1e81755e9bceb936c1f792234afd648
evahoralikova <eva.horalikova@gmail.com>
2022-09-21 15:24:19 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+/* UTF-8 codepage: Příliš žluťoučký kůň úpěl ďábelské ódy. ÷ × ¤
+ * «Stereotype», Section mark-§, Copyright-©, Alpha-α, Beta-β, Smile-☺
+ */
+
+
+
+
+/*******************************************************************************
+ * Třída {@code Main} je hlavní třídou projektu,
+ * který ...
+ *
+ * @author  author name
+ * @version 0.00.0000 — 20yy-mm-dd
+ */
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+       int celé = 546_71;
+       var real = 3.14;
+       var real2 = 3.14e2;
+       short malé = 1234;
+       char znak = 1234;
+       char znak2 = (char)(znak + 7);
+        System.out.println("==========================================\n"
+        +"celé = " + celé
+        + "\nReal = "+ real
+        + "\nReal2 = "+ real2
+        + "\nMalé = "+ malé
+        + "\nZnak = "+ znak
+        + "\nZnak2 = "+ znak2);
+    }
+    public static void main2()
+    {
+        System.out.println("==========================================\n"
+        + "\n0b1_0111 = " + 0b1_0111
+        + "\n0x1_fF = " + 0xfF);
+    }
+}
